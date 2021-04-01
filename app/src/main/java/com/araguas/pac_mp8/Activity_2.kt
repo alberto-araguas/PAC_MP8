43ff84e23a7b42c34c1d01136c083d9d79b2779d
AlbertoA <alberto.araguas@gmail.com>
2021-04-01 20:54:42 +0200

    Todas las Activities creadas pero vacias

---
+++ app/src/main/java/com/araguas/pac_mp8/Activity_2.kt
@@ -0,0 +1,12 @@
+package com.araguas.pac_mp8
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
---
+++ app/src/main/java/com/araguas/pac_mp8/Activity_4.kt
@@ -0,0 +1,12 @@
+package com.araguas.pac_mp8
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_4)
+    }
+}
