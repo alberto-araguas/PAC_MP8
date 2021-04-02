b4e8ae7bc99748758d4b7be9ffb436e562cc49a4
AlbertoA <alberto.araguas@gmail.com>
2021-04-02 19:51:10 +0200

    bobon camara

--- app/src/main/java/com/araguas/pac_mp8/Activity_3.kt
+++ app/src/main/java/com/araguas/pac_mp8/Activity_3.kt
@@ -1,12 +1,29 @@
 package com.araguas.pac_mp8
 
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_3.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.btnIrActivity4
 
 class Activity_3 : AppCompatActivity() {
+    //trabajar sobre la camara
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_3)
+        //boton volver Activity 1
+        BtnAct3Volver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    // función que devuelve true  si el terminal tiene camara
+    private fun checkCameraHardware(context: Context): Boolean {
+        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
     }
 }
