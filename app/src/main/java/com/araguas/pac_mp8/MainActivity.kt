fc0d48a62e7ff5e5fb78b3cf116bcf65b96aab8d
AlbertoA <alberto.araguas@gmail.com>
2021-04-02 10:31:25 +0200

    botones activity main funcionando

--- app/src/main/java/com/araguas/pac_mp8/MainActivity.kt
+++ app/src/main/java/com/araguas/pac_mp8/MainActivity.kt
@@ -9,10 +9,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+    // configuramos las acciones de los botones
           btnIrActivity2.setOnClickListener {
            val intent = Intent(this, Activity_2::class.java)
            startActivity(intent)
+          }
+          btnIrActivity3.setOnClickListener {
+            val intent = Intent(this, Activity_3::class.java)
+            startActivity(intent)
+          }
+          btnIrActivity4.setOnClickListener {
+            val intent = Intent(this, Activity_4::class.java)
+            startActivity(intent)
           }
 
     }
