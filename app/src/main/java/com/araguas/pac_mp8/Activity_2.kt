ca86435890841832f4df634aa5feaeab98d4fd72
AlbertoA <alberto.araguas@gmail.com>
2021-04-02 10:09:48 +0200

    botones activity main funcionando

--- app/src/main/java/com/araguas/pac_mp8/Activity_2.kt
+++ app/src/main/java/com/araguas/pac_mp8/Activity_2.kt
@@ -1,12 +1,18 @@
 package com.araguas.pac_mp8
 
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_2.*
 
 class Activity_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        botonprueba.setOnClickListener{
+            //La acción que quisiéramos hacer
+        }
     }
 }
--- app/src/main/java/com/araguas/pac_mp8/MainActivity.kt
+++ app/src/main/java/com/araguas/pac_mp8/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.araguas.pac_mp8
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+          btnIrActivity2.setOnClickListener {
+           val intent = Intent(this, Activity_2::class.java)
+           startActivity(intent)
+          }
+
     }
+
+
 }
