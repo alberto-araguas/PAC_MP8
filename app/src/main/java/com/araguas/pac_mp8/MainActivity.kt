ab63767b4b3d2a8f51bf2f8382196cc55664713a
AlbertoA <alberto.araguas@gmail.com>
2021-04-04 21:10:20 +0200

    activity1 terminada com mediaplayer

--- app/src/main/java/com/araguas/pac_mp8/MainActivity.kt
+++ app/src/main/java/com/araguas/pac_mp8/MainActivity.kt
@@ -2,9 +2,12 @@ package com.araguas.pac_mp8
 
 import android.content.Intent
 import android.media.AudioManager
+import android.media.MediaPlayer
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,11 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //inicializamos soundPool cangando el archivo de música
-        soundPool =  SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool!!.load(baseContext, R.raw.golden, 1)
-
        // configuramos las acciones de los botones de acceso a otra Activity
           btnIrActivity2.setOnClickListener {
            val intent = Intent(this, Activity_2::class.java)
@@ -38,20 +36,32 @@ class MainActivity : AppCompatActivity() {
           btnStop.setOnClickListener {
             stopSound()
           }
+        mPlayer=MediaPlayer.create(this,R.raw.golden)
+
     }
 
+   //Para la realización del reproductor se ha utlizado el mediaPlayer en vez del soundPool, ya
+   // que ANDROID STUDIO indica que soundPool está en desuso y creo que es mejor utilizar funcionalidades
+   // que no estén descontinuadas.
 
-
-    //variables que usa sounPool
-    private var soundPool: SoundPool? = null
-    private val soundId = 1
+    var mPlayer: MediaPlayer? = null
 
     private fun playSound() {
-        soundPool?.play(soundId, 1F, 1F, 1, 1, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+        //en un bloque try-cacth por seguridad, realizamos la reproducción  con el método .start
+        try {
+            mPlayer?.start()
+            Toast.makeText(this, "Canción sin derechos de autor", Toast.LENGTH_LONG).show()
+        }catch(e: Exception ){
+            Log.e("musica", "error de reproduccion")
+        }
     }
     private fun stopSound() {
-        soundPool?.stop(soundId)
-    }
+        // pausamos la cancion y la ponermos al principio
+        mPlayer?.seekTo(0)
+        mPlayer?.pause()
+   }
+
+
+
 
 }
