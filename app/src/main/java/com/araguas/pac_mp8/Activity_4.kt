d57b70e0ae1c016da1c2e3fc5c1b34cf9710de8d
AlbertoA <alberto.araguas@gmail.com>
2021-04-05 11:15:40 +0200

    Layout vertical y boton volver

--- app/src/main/java/com/araguas/pac_mp8/Activity_4.kt
+++ app/src/main/java/com/araguas/pac_mp8/Activity_4.kt
@@ -1,12 +1,20 @@
 package com.araguas.pac_mp8
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_3.*
+import kotlinx.android.synthetic.main.activity_4.*
 
 class Activity_4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_4)
+
+        btnVolverA1.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
