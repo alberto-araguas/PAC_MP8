816914c13362f2634ecef75fd0e855baf5dd4467
AlbertoA <alberto.araguas@gmail.com>
2021-04-09 18:03:21 +0200

    ACTIVITY 2 con los botones creados

--- app/src/main/java/com/araguas/pac_mp8/Activity_2.kt
+++ app/src/main/java/com/araguas/pac_mp8/Activity_2.kt
@@ -2,8 +2,10 @@ package com.araguas.pac_mp8
 
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_2.*
 
 class Activity_2 : AppCompatActivity() {
@@ -11,8 +13,14 @@ class Activity_2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
-        botonprueba.setOnClickListener{
-            //La acción que quisiéramos hacer
+        btnVoverAct1.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "Entrando en Activity 2", Toast.LENGTH_LONG).show()
     }
 }
