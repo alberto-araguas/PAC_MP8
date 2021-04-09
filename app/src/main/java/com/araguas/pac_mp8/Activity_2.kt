c168e6fcd88de3c670729d92c88cf5c53d7c0c75
AlbertoA <alberto.araguas@gmail.com>
2021-04-09 19:29:22 +0200

    todo funcional excepto la creacion de tablas

--- app/src/main/java/com/araguas/pac_mp8/Activity_2.kt
+++ app/src/main/java/com/araguas/pac_mp8/Activity_2.kt
@@ -2,7 +2,9 @@ package com.araguas.pac_mp8
 
 
 
+import android.content.ContentValues
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -12,6 +14,42 @@ class Activity_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        btnTabla.setOnClickListener{
+            crearTabla()
+
+        }
+
+        btnInsertar.setOnClickListener{
+            val admin = AdminSQLiteOpenHelper(this,"administracion", null, 1)
+            val bd = admin.writableDatabase
+            val registro = ContentValues()
+            registro.put("dni", etDni.getText().toString())
+            registro.put("nombre", etNombre.getText().toString())
+            registro.put("correo", etCorreo.getText().toString())
+            bd.insert("personas", null, registro)
+            bd.close()
+            etDni.setText("")
+            etNombre.setText("")
+            etCorreo.setText("")
+            Toast.makeText(this, "Se cargaron los datos de la persona", Toast.LENGTH_SHORT).show()
+        }
+
+        btnConsultar.setOnClickListener{
+            try {
+                val admin = AdminSQLiteOpenHelper(this, "administracion", null, 1)
+                val bd = admin.writableDatabase
+                val fila = bd.rawQuery("select nombre, correo from personas where dni=${etDni.text}", null)
+                if (fila.moveToFirst()) {
+                    etNombre.setText(fila.getString(0))
+                    etCorreo.setText(fila.getString(1))
+                } else
+                    Toast.makeText(this, "No existe un artículo con dicho código", Toast.LENGTH_SHORT).show()
+                bd.close()
+            }catch ( exception: Exception){
+                Toast.makeText(this, "Error en la lectura de datos de la BBDD", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         btnVoverAct1.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
@@ -23,4 +61,19 @@ class Activity_2 : AppCompatActivity() {
         super.onStart()
         Toast.makeText(this, "Entrando en Activity 2", Toast.LENGTH_LONG).show()
     }
+
+    fun crearTabla(){
+        //lanzamos el comando SQL que crea la tabla
+        val admin = AdminSQLiteOpenHelper(this, "administracion", null, 1)
+        val bd = admin.writableDatabase
+        val tabla = bd.rawQuery("CREATE TABLE personas(dni String primary key, nombre String, correo String)", null)
+    }
+
+
+
+
+
+
 }
+
+
---
+++ app/src/main/java/com/araguas/pac_mp8/AdminSQLiteOpenHelper.kt
@@ -0,0 +1,17 @@
+package com.araguas.pac_mp8
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class AdminSQLiteOpenHelper (context: Context, name: String, factory: SQLiteDatabase.CursorFactory?, version: Int) : SQLiteOpenHelper (context,name,factory,version){
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("create table personas(num text primary key, nombre text, correo text)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+}
